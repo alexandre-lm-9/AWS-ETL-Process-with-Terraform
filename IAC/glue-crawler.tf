185022dc50b8172f8fa05bbfd590f7c178cd36f2
alexandre-lm-9 <alombardom@hotmail.com>
2023-02-04 00:43:10 -0300

    Updating preffix table

--- IAC/glue-crawler.tf
+++ IAC/glue-crawler.tf
@@ -7,7 +7,7 @@ resource "aws_glue_crawler" "glue-crawler" {
   name          = "glue-crawler-tf"
   role          = aws_iam_role.glue-crawler-role.arn
 
-  table_prefix = "rais-2020-"
+  table_prefix = "rais_2020_"
 
   s3_target {
     path = "s3://${aws_s3_bucket.datalake_tf.id}/${aws_s3_bucket_object.tf-staging.id}"
